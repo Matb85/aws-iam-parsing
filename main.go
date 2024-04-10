3c0d7d39e432a38159edeabc253ef4d6a9776e09
matb85 <mateuszbis85@gmail.com>
2024-04-10 09:38:31 +0200

    feat: :sparkles: update main.go so it calls VerifyPolicyJSON with data from data.json

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"matb85/remitly-home-assignment/method"
+	"os"
+)
 
-func main (){
-	fmt.Println("Hello from template")
+func main() {
+	data, err := os.ReadFile("data.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	result := method.VerifyPolicyJSON(data)
+	fmt.Println((result))
 }
